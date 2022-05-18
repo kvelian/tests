f10a5a74bd370f6560dcb89db004d4e4640f51a6
GeroyDonbassa <prplnthtch@gmail.com>
2022-05-18 18:46:25 +0700

    create refund tests

--- utils/constants/tests/texts/index.ts
+++ utils/constants/tests/texts/index.ts
@@ -1,1 +1,2 @@
 export * from './validation/validation'
+export * from './transferStatuses'
---
+++ utils/constants/tests/texts/transferStatuses.ts
@@ -0,0 +1,7 @@
+export const TRANSFER_STATUS = {
+    READY: "Готов к выдаче",
+    RECEIVED_ON_CARD: "Отправлен на карту",
+    REFUNDED: "Оформлен возврат",
+    BLOCKED_FOR_REVIEW: "На проверке"
+
+}
